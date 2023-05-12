80df508a2cbbfb0a27c607ffb9ef7ccf1de04c64
varunkas <varun.sreenivasan@outlook.com>
2023-05-12 18:29:33 +0200

    Add name to bedfile

--- vcfgz_to_bed.sh
+++ vcfgz_to_bed.sh
@@ -1,7 +1,9 @@
-# submit as vcfgz_to_bed.sh <vcfgzfile>
+# submit as vcfgz_to_bed.sh <vcfgzfile> <name>
 set -e
 
 file=$(basename $1 ".vcf.gz")
+name=$2
+
 cd $(dirname $1)
 
 source /Users/sreenivasan/opt/anaconda3/etc/profile.d/conda.sh
@@ -19,12 +21,15 @@ cut -f1,2,6,11 tmp_${file}.bed | grep -E 'DEL|INS|DUP' | grep -v -E '_random|_al
 # Sort bed file
 bedtools sort -i ${file}.bed > ${file}.sorted.bed && rm ${file}.bed
 
-# split into multiple, as needed
-split -l 9000 -d ${file}.sorted.bed "${file}.sorted_chunk"
-for tmp_file in ${file}.sorted_chunk*
-do
-    mv "$tmp_file" "${tmp_file}.bed"
-done
+#add name as the 5th column
+awk -v name=$name 'BEGIN {OFS="\t"} {$5 = name; print}' ${file}.sorted.bed > ${file}.sorted.named.bed && rm ${file}.sorted.bed
 
-mkdir -p bedfiles
-mv *${file}*.bed bedfiles/
+# split into multiple, as needed
+# split -l 9000 -d ${file}.sorted.bed "${file}.sorted_chunk"
+# for tmp_file in ${file}.sorted_chunk*
+# do
+#    mv "$tmp_file" "${tmp_file}.bed"
+# done
+
+mkdir -p bedfiles_for_caddsv
+mv *${file}*.bed bedfiles_for_caddsv/
