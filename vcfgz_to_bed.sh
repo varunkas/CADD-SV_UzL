af813f077e5b071980126c239288d1daecd41875
varunkas <varun.sreenivasan@outlook.com>
2023-05-12 14:01:01 +0200

    Add bedtools sorting

--- vcfgz_to_bed.sh
+++ vcfgz_to_bed.sh
@@ -12,11 +12,15 @@ gunzip "${file}.vcf.gz"
 
 # convert to bedfile and extract required columns for CADD-SV. Keep only DEL, DUP, and INS
 SURVIVOR vcftobed ${file}.vcf 0 -1 tmp_${file}.bed
+
 cut -f1,2,6,11 tmp_${file}.bed | grep -E 'DEL|INS|DUP' | grep -v -E '_random|_alt|chrUn|chrM'  > ${file}.bed && rm tmp_${file}.bed
 
+# Sort bed file
+bedtools sort -i ${file}.bed > ${file}.sorted.bed && rm ${file}.bed
+
 # split into multiple, as needed
-split -l 9000 -d ${file}.bed "${file}_chunk"
-for tmp_file in ${file}_chunk*
+split -l 9000 -d ${file}.sorted.bed "${file}.sorted_chunk"
+for tmp_file in ${file}.sorted_chunk*
 do
     mv "$tmp_file" "${tmp_file}.bed"
 done
